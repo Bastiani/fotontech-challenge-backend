56ab01c653b7d8b37e8c6f01c2edc430a9245326
Bastiani <rafacdb@gmail.com>
2019-07-29 18:19:22 -0300

    feat(database): update database url

--- src/database.ts
+++ src/database.ts
@@ -8,6 +8,7 @@ export default function connectDatabase(): Promise<Mongoose> {
       .on('error', error => reject(error))
       .on('close', () => console.log('Database connection closed.'))
       .once('open', () => resolve(mongoose.connections[0]));
-    mongoose.connect('mongodb://admin:mongo2019@localhost:27017/nodejs-login-boilerplate?authSource=admin', { useNewUrlParser: true });
+    // mongoose.connect('mongodb://admin:mongo2019@localhost:27017/nodejs-login-boilerplate?authSource=admin', { useNewUrlParser: true });
+    mongoose.connect('mongodb://localhost/bastiani-challenge', { useNewUrlParser: true });
   });
 }
